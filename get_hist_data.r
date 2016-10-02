92b8edb51180e83d91b2b399f39db615f9e34179
Evan Kaeding <evan.kaeding@hotmail.com>
2016-10-02 11:46:20 -0700

    Adding the get_hist_data.r file to this repo

---
+++ get_hist_data.r
@@ -0,0 +1,46 @@
+##This function is designed to pull down historical stock return
+##information from Yahoo Finance's API. The function takes the number
+##of years requested, the frequency of reporting and the ticker as arguments, 
+##downloads a csv and returns the contents of the csv as a data.frame
+
+get_hist_data <- function(years, tkr, freq){
+  
+  ##date2 represents the date the URL API form
+  
+  today <- Sys.Date()
+  
+  date2 <- paste("&d=", format(today, '%m'), "&e=", format(today, '%d'), "&f=", format(today, '%Y'), sep='')
+  
+  
+  ##date1 represents the date in the URL API form
+  
+  start <- today - years * 365
+  
+  date1 <- paste("&a=", format(start, '%m'), "&b=", format(start, '%d'), "&c=", format(start, '%Y'), sep='')
+  
+  ##Frequency dictates whether the API will give daily, weekly, monthly or
+  ##dividend only data. d = daily, w = weekly, m = monthly , v = dividends only
+  
+  frequency <- paste("&g=", freq, sep='')
+  
+  
+  ##Start Building the URL
+  
+  domain_path <- "http://real-chart.finance.yahoo.com/table.csv?s="
+  
+  tail <- "&ignore=.csv"
+  
+  ##Combine all of the parts into the link
+  
+  link <- paste(domain_path, tkr, date1, date2, frequency, tail, sep='')
+  
+  ##Download the file and name it "[stock].csv"
+  
+  download.file(link, paste(tkr, "-", freq, ".csv", sep=''))
+  
+  ##Return a data.frame that has the stock history
+                            
+  data.frame(read.csv(paste(tkr, "-", freq, ".csv", sep='')))
+  
+  
+}
